086397fca427a48eb54df0ffcba891bbaafa2cf3
DevJo <sa02045@naver.com>
2021-05-18 20:39:31 +0900

    2021-05-18 TIL

--- docs/.vuepress/const.js
+++ docs/.vuepress/const.js
@@ -1,1 +1,3 @@
-module.exports.AlogoList = ["Algorithm/C++입력.md"]
+module.exports.AlogoList = ["Algorithm/C++입력.md", "Algorithm/C++배열선언.md"]
+module.exports.RedisList = ["Redis/디디테코톡정리.md"]
+module.exports.WebList = ["Web/Websocket.md"]
