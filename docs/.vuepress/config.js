c66d65fcc7f4ac4f54d769f61e5cbed6e5974a7a
DevJo <sa02045@naver.com>
2021-08-18 13:15:30 +0900

    chore: List 추가

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-var CONST = require("./const.js")
+var CONST = require("./const.js");
 
 module.exports = {
   title: "물처럼 흐르는 기억을 바위에 새기듯 글로 쓰자",
@@ -10,6 +10,30 @@ module.exports = {
         title: "알고리즘",
         children: CONST.AlogoList,
       },
+      {
+        title: "Express",
+        children: CONST.ExpressList,
+      },
+      {
+        title: "HTTP",
+        children: CONST.HttpList,
+      },
+      {
+        title: "login",
+        children: CONST.loginList,
+      },
+      {
+        title: "NodeJS",
+        children: CONST.NodeJsList,
+      },
+      {
+        title: "React",
+        children: CONST.ReactList,
+      },
+      {
+        title: "Webpack",
+        children: CONST.webpackList,
+      },
     ],
   },
-}
+};
--- docs/.vuepress/const.js
+++ docs/.vuepress/const.js
@@ -1,3 +1,15 @@
-module.exports.AlogoList = ["Algorithm/C++입력.md", "Algorithm/C++배열선언.md"]
-module.exports.RedisList = ["Redis/디디테코톡정리.md"]
-module.exports.WebList = ["Web/Websocket.md"]
+module.exports.AlogoList = ["Algorithm/C++입력.md", "Algorithm/C++배열선언.md"];
+module.exports.RedisList = ["Redis/디디테코톡정리.md"];
+module.exports.WebList = ["Web/Websocket.md"];
+module.exports.ExpressList = ["Express/cookie-parser.md"];
+module.exports.HttpList = ["Http/HTTPS.md"];
+module.exports.NodeJsList = ["NodeJS/process.md"];
+module.exports.ReactList = ["React/디자인패턴.md", "React/커스텀훅.md"];
+module.exports.loginList = [
+  "login/인증과인가.md",
+  "login/JWT.md",
+  "login/kakao.md",
+];
+
+module.exports.webpackList = ["webpack/정리.md"];
+module.exports.GraphQlLIst = ["GraphQL/tutorial.md"];
